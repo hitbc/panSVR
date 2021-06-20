d18ae904a2040c554045c3917f35f333079746e3
Gaoyang Li <gaoyangli@hit.edu.cn>
2021-06-20 16:01:52 +0800

    Add files via upload

---
+++ Release/src/cpp_lib/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/cpp_lib/graph.cpp 
+
+OBJS += \
+./src/cpp_lib/graph.o 
+
+CPP_DEPS += \
+./src/cpp_lib/graph.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/cpp_lib/%.o: ../src/cpp_lib/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/home/fenghe/eclipse-workspace/panSV/src/htslib" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
